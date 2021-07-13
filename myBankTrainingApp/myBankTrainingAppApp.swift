ea90fbe0f6addef4e9f7695b452add7d5437b2bb
Felipe Gomez <fiatdv>
2021-07-13 10:43:20 -0400

    Initial Commit

---
+++ myBankTrainingApp/myBankTrainingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myBankTrainingAppApp.swift
+//  myBankTrainingApp
+//
+//  Created by Felipe on 7/13/21.
+//
+
+import SwiftUI
+
+@main
+struct myBankTrainingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
